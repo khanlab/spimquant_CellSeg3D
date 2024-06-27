c24e5cc881ba949731672698f46efdb0e877956e
Karl5766 <karl29029@gmail.com>
2024-06-27 14:25:08 -0400

    bug fix

--- spimquant.py
+++ spimquant.py
@@ -2,7 +2,161 @@ from napari_cellseg3d.code_models.models.wnet.model import WNet
 import torch
 from cvpl_tools.dataset_reference import DatasetReference
 from dataclasses import dataclass
-import argparse
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import dask.array as da
+import cvpl_tools.fs as fs
+import cvpl_tools.dataset_reference as dataset_reference
+from cvpl_tools.array_key_dict import ArrayKeyDict
+import cvpl_tools.persistence as persistence
+import cvpl_tools.strenc as strenc
+
+
+csconf = snakemake.params.cellsegment
+DEVICE = csconf['device']
+DATASET_NAME = csconf['dataset_name']
+SCRATCH_FOLDER = csconf['scratch_dir']
+IM_CHANNEL = csconf['im_channel']
+def load_OME_ZARR_as_zarr_group(path):
+    import gcsfs
+    import zarr
+    gfs = gcsfs.GCSFileSystem(token=None)
+    store = gfs.get_mapper(path)
+    zarr_group = zarr.open(store, mode='r')
+    return zarr_group
+
+
+def main():
+    import sys
+    sys.path.append('../../..//spimquant/submodules/spimquant_cellpose')
+    cmd = snakemake.params.command
+    if cmd == 'init_dataset':
+        init_dataset()
+    elif cmd == 'train':
+        train()
+    elif cmd == 'predict':
+        inference()
+    else:
+        raise ValueError(f'ERROR: Snakemake command is not recognized: {snakemake.command}')
+
+
+def init_dataset():
+    dataset_dir = snakemake.output.dataset_dir
+    fs.ensure_dir_exists(dataset_dir, True)
+    TOTAL_N = csconf["num_train_chunk"]  # total number of splits we want in the end
+    CREATION_INFO = csconf["creation_info"]
+    zarr_group = load_OME_ZARR_as_zarr_group(snakemake.params.zarr)
+    print(list(zarr_group.keys()))
+    zarr_subgroup = zarr_group['0']
+
+    zarr_shape = np.array(zarr_subgroup.shape, dtype=np.int32)
+    print(zarr_shape)
+    LD_SIZES = np.array(csconf["ld_size"], dtype=np.int32)
+    SPLIT_SIZES = np.array(csconf["train_chunk_size"], dtype=np.int32)
+    LD_BY_SPLIT = LD_SIZES // SPLIT_SIZES
+    LOOP_PER_LD = csconf["num_chunk_per_ld"]  # from each LD slice, we attempt to read this many splits
+
+    slice_arr = ArrayKeyDict()
+    LD_RNG = ((zarr_shape[1:] - 1) // LD_SIZES) + 1
+    READ_N = 0
+    im = np.zeros((2, LD_SIZES[0], LD_SIZES[1], LD_SIZES[2]), dtype=np.uint16)
+    while True:
+        rng = np.random.randint(0, LD_RNG)
+        ld_start = rng * LD_SIZES
+        ld_end = ld_start + LD_SIZES
+        result_im = da.from_zarr(zarr_subgroup)[:, ld_start[0]:ld_end[0], ld_start[1]:ld_end[1], ld_start[2]:ld_end[2]]
+        result_im = result_im.compute()
+        im[:result_im.shape[0], :result_im.shape[1], :result_im.shape[2], :result_im.shape[3]] = result_im
+        print(f'Computed slice at location {ld_start}')
+        for i in range(LOOP_PER_LD):
+            rng2 = np.random.randint(0, LD_BY_SPLIT)
+            split_start = rng2 * SPLIT_SIZES
+            global_start = ld_start + split_start
+            imid = f'{global_start[0].item()}_{global_start[1].item()}_{global_start[2].item()}'
+            if imid in slice_arr:
+                continue
+            split_end = split_start + SPLIT_SIZES
+            split = im[:, split_start[0]:split_end[0], split_start[1]:split_end[1], split_start[2]:split_end[2]]
+            split = np.clip(split, 0, 1000)
+            if np.var(split[IM_CHANNEL]) < 1000.:  # too little variation, really nothing is here
+                continue
+
+            save_path = f'{dataset_dir}/{imid}.npy'
+            slice_arr[imid] = dataset_reference.DatapointReference(save_path)
+            READ_N += 1
+            np.save(save_path, split)
+            if READ_N >= TOTAL_N:
+                break
+        print(f'Finished sampling splits from slice with READ_N={READ_N}')
+        if READ_N >= TOTAL_N:
+            break
+    im_read_setting = fs.ImReadSetting(
+        true_im_ndim=4,
+        im_format=fs.ImFileType.FORMAT_UINT16)
+    ref = dataset_reference.DatasetReference.new(
+        slice_arr,
+        DATASET_NAME,
+        CREATION_INFO,
+        im_read_setting
+    )
+    persistence.write_dataset_reference(ref, f'{dataset_dir}/DatasetReference')
+
+
+def train():
+    import logging
+    from napari_cellseg3d.dev_scripts.colab_training import logger
+    import train
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    dataset_dir = snakemake.input.dataset_dir
+    trainset = persistence.read_dataset_reference(f'{dataset_dir}/DatasetReference')
+    model_weight_dir = os.path.abspath(f'{snakemake.params.output_dir}/{DATASET_NAME}_model_weight')
+    fs.ensure_dir_exists(model_weight_dir, True)
+    scratch_dir = f'{SCRATCH_FOLDER}/{DATASET_NAME}'
+    fs.ensure_dir_exists(scratch_dir, True)
+    config = {
+        'trainset': trainset,
+        'scratch_folder': scratch_dir,
+        'result_folder': model_weight_dir,
+        'im_channel': IM_CHANNEL,
+        'num_classes': csconf['num_classes'],
+        'input_brightness_range': (csconf['min_brightness'], csconf['max_brightness']),
+        'nepochs': csconf['nepochs'],
+        'n_cuts_weight': csconf['n_cuts_weight'],
+        'rec_loss_weight': csconf['rec_loss_weight'],
+        'device': csconf['device']
+    }
+    config = train.train_model(config)
+    persistence.write_dict(config, snakemake.output.model_config)
+
+
+def inference():
+    import predict
+    loc = (1024, 1888, 992)
+    zarr_group = load_OME_ZARR_as_zarr_group(snakemake.params.zarr)
+    zarr_subgroup = zarr_group['0']
+    im = da.from_zarr(zarr_subgroup)[IM_CHANNEL, loc[0]:loc[0] + 32,
+                loc[1]:loc[1] + 2048, loc[2]:loc[2] + 2048]
+
+    model_config = persistence.read_dict(snakemake.input.model_config)
+    output = predict.inference_on_np3d(model_config, im, [32, 32, 32])
+    print('finished inference')
+    output = output.detach().cpu().numpy()[0]
+    print('output extracted')
+    output_argmax = output.argmax(axis=0)
+    print('argmax computed')
+    result_dir = snakemake.output.pred_result_dir
+    fs.ensure_dir_exists(result_dir, True)
+    np.save(f'{result_dir}/inference.npy', output)
+    print('output written')
+    np.save(f'{result_dir}/inference_argmax.npy', output_argmax)
+    print('argmax written')
+    np.save(f'{result_dir}/inference_im.npy', im)
+    print('inference image written')
 
 
 @dataclass
@@ -36,4 +190,8 @@ def create_model(config: CellSeg3DModelConfig):
     weights = torch.load(config.model_weight_path, map_location=config.device)
     model.load_state_dict(weights, strict=True)
     return model
+
+
+if __name__ == '__main__':
+    main()
 
--- train.py
+++ train.py
@@ -6,57 +6,6 @@ from cvpl_tools.dataset_reference import DatasetReference
 import numpy as np
 import os, shutil
 from pathlib import Path
-import argparse
-import cvpl_tools.strenc as strenc
-import json
-
-
-def main():
-    config = load_config_from_argparse_for_training()
-    train_model(config)
-
-
-def load_config_from_argparse_for_training() -> CellSeg3DModelConfig:
-    parser = argparse.ArgumentParser(description='Example with dictionary')
-    parser.add_argument('--im_channel', type=int, default=0,
-                        help='The channel of input image to train CellSeg3D on')
-    parser.add_argument('--trainset', type=str, default=None,
-                        help='path to the dataset reference file; this is the training set to use')
-    parser.add_argument('--min_brightness', type=float, default=0.,
-                        help='The minimum brightness threshold to clip the input image into')
-    parser.add_argument('--max_brightness', type=float, default=1000.,
-                        help='The maximum brightness threshold to clip the input image into')
-    parser.add_argument('--n_cuts_weight', type=float, default=.5,
-                        help='Weighting of one of the two losses used by CellSeg3D: n cuts loss')
-    parser.add_argument('--rec_loss_weight', type=float, default=.005,
-                        help='Weighting of one of the two losses used by CellSeg3D: reconstruction loss')
-    parser.add_argument('--nepoch', type=int, default=20,
-                        help='The number of epochs to train for')
-    parser.add_argument('--num_classes', type=int, default=.005,
-                        help='The number of output class in the autoencoder representation of CellSeg3D')
-    parser.add_argument('--scratch_folder', type=str, default=None,
-                        help='Path to the scratch folder that can be used for storing intermediate input files.')
-    parser.add_argument('--result_folder', type=str, default=None,
-                        help='Path to the result folder that can be used for storing result files (model weights etc.)')
-    parser.add_argument('--device', type=str, default='cpu', help='device to use for training')
-
-    args = parser.parse_args()
-    with open(args.trainset, 'r') as infile:
-        ref: DatasetReference = json.load(infile, object_hook=strenc.get_decoder_hook())
-    config = CellSeg3DModelConfig(
-        trainset=ref,
-        scratch_folder=args.scratch_folder,
-        result_folder=args.result_folder,
-        im_channel=args.im_channel,
-        input_brightness_range=(args.min_brightness, args.max_brightness),
-        n_cuts_weight=args.n_cuts_weight,
-        rec_loss_weight=args.rec_loss_weight,
-        nepochs=args.nepochs,
-        num_classes=args.num_classes,
-        device=args.device,
-    )
-
-    return config
 
 
 def preprocess_to_3d_tiles(dataset_ref: DatasetReference,
@@ -75,18 +24,18 @@ def preprocess_to_3d_tiles(dataset_ref: DatasetReference,
         np.save(f'{out_prefix}/{imid}.npy', im)
 
 
-def train_model(config: CellSeg3DModelConfig):
-    PROCESSED_SLICE_FOLDER = f'{config.scratch_folder}/cellseg3d_train_inputs'
+def train_model(config: dict):
+    PROCESSED_SLICE_FOLDER = f'{config["scratch_folder"]}/cellseg3d_train_inputs'
     if os.path.exists(PROCESSED_SLICE_FOLDER):
         shutil.rmtree(PROCESSED_SLICE_FOLDER)
     os.mkdir(PROCESSED_SLICE_FOLDER)
-    if os.path.exists(config.result_folder):
-        shutil.rmtree(config.result_folder)
-    os.mkdir(config.result_folder)
+    if os.path.exists(config["result_folder"]):
+        shutil.rmtree(config["result_folder"])
+    os.mkdir(config["result_folder"])
 
-    clamp_min, clamp_max = config.input_brightness_range
-    preprocess_to_3d_tiles(config.trainset, PROCESSED_SLICE_FOLDER,
-                                 config.im_channel,
+    clamp_min, clamp_max = config["input_brightness_range"]
+    preprocess_to_3d_tiles(config["trainset, PROCESSED_SLICE_FOLDER"],
+                                 config["im_channel"],
                                  clamp_max, clamp_min)
 
     batch_size = 4
@@ -108,29 +57,29 @@ def train_model(config: CellSeg3DModelConfig):
 
     global train_config, wandb_config
     train_config = WNetTrainingWorkerConfig(
-        device=config.device,
-        max_epochs=config.nepochs,
+        device=config["device"],
+        max_epochs=config["nepochs"],
         learning_rate=learning_rate,
         validation_interval=validation_frequency,
         batch_size=batch_size,
         num_workers=2,
         weights_info=WeightsInfo(),
-        results_path_folder=config.result_folder,
+        results_path_folder=config["result_folder"],
         train_data_dict=create_dataset(train_data_folder),
         eval_volume_dict=None,
         # advanced
-        in_channels=config.in_channels,
-        out_channels=config.out_channels,
-        num_classes=config.num_classes,
-        dropout=config.dropout,
+        in_channels=config["in_channels"],
+        out_channels=config["out_channels"],
+        num_classes=config["num_classes"],
+        dropout=config["dropout"],
         weight_decay=weight_decay,
         intensity_sigma=intensity_sigma,
         spatial_sigma=spatial_sigma,
         radius=ncuts_radius,
-        reconstruction_loss=config.rec_loss,
-        n_cuts_weight=config.n_cuts_weight,
-        rec_loss_weight=config.rec_loss_weight,
-        input_brightness_range=config.input_brightness_range,
+        reconstruction_loss=config["rec_loss"],
+        n_cuts_weight=config["n_cuts_weight"],
+        rec_loss_weight=config["rec_loss_weight"],
+        input_brightness_range=config["input_brightness_range"],
     )
     wandb_config = WandBConfig(
         mode="disabled" if not WANDB_INSTALLED else "online",
@@ -140,9 +89,5 @@ def train_model(config: CellSeg3DModelConfig):
     worker = c.get_colab_worker(worker_config=train_config, wandb_config=wandb_config)
     for epoch_loss in worker.train():
         continue
-    config.model_weight_path = f'{config.result_folder}/wnet.pth'
+    config["model_weight_path"] = f'{config["result_folder"]}/wnet.pth'
     return config
-
-
-if __name__ == '__main__':
-    main()
